95db184c27a95f5c1231f7b71cce8986217d0b70
Izabela <izabela.hent@gmail.com>
2023-03-30 16:31:31 +0300

    adding the first test file

---
+++ Test1.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+describe('First test', () => {
+
+    it('clicks on Contact us and tests the form', () => {
+
+        cy.visit('http://webdriveruniversity.com/index.html')
+
+    })
+
+})
